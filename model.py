f40342ff6d963888f191368e8fa81140a250a180
Yogesh Ankolekar <yogeshankolekar@gmail.com>
2021-10-18 22:38:42 +0530

    updated file
    updated file

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ import xgboost
 
 
 data  = pkl.load(open('dataset/data.pkl','rb'))
-xgbc        = pkl.load(open('models/XGBoost.pkl','rb'))
+xgbc2        = pkl.load(open('models/XGBoost.pkl','rb'))
 tfidf      = pkl.load(open('models/tfidf_features.pkl','rb'))
 user_recom = pkl.load(open('models/user_recommendation.pkl','rb'))
 
@@ -23,7 +23,7 @@ user_recom = pkl.load(open('models/user_recommendation.pkl','rb'))
 def sentiment(recom_prod):
     df = data[data.name.isin(recom_prod)]
     tfidf_features = tfidf.transform(df['text'])
-    pred_data = xgbc.predict(tfidf_features)
+    pred_data = xgbc2.predict(tfidf_features)
     predictions = [round(value) for value in pred_data]
     df['predict'] = predictions
     output_data = df[df['predict']==1][['name',    'brand', 'categories']].drop_duplicates()[:5].reset_index(drop=True)
