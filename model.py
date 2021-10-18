b51c9c91631972b788e3c2f5819fc1f3eb7a3fc5
Yogesh Ankolekar <yogeshankolekar@gmail.com>
2021-10-18 22:25:07 +0530

    updated
    updated

--- model.py
+++ model.py
@@ -25,16 +25,11 @@ def sentiment(recom_prod):
     tfidf_features = tfidf.transform(df['text'])
     pred_data = xgbc.predict(tfidf_features)
     predictions = [round(value) for value in pred_data]
-    df['predicted'] = predictions
-    output_data = df[df['predicted']==1][['name',    'brand', 'categories']].drop_duplicates()[:5].reset_index(drop=True)
+    df['predict'] = predictions
+    output_data = df[df['predict']==1][['name',    'brand', 'categories']].drop_duplicates()[:5].reset_index(drop=True)
 
     return output_data
 
 def recommendation(user_input):
     recom = user_recom.loc[user_input.lower()].sort_values(ascending=False)[0:20].index
     return recom
-
-
-# Take the user ID as input.
-# user_input = input("Enter your user name")
-# print(user_input)
